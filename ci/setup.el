1a421fb37514bc76fc0378edf533d902981448ea
Katharina Fey <kookie@spacekookie.de>
2021-11-10 18:22:53 +0100

    Initial CI

---
+++ ci/setup.el
@@ -0,0 +1,12 @@
+;; Evaluate this file to set-up emacs in CI for building slides
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(package-initialize)
+(setq package-selected-packages
+      '(org-mode
+        ox-reveal
+        htmlify))
+(package-install-selected-packages)
+
+(save-buffers-kill-emacs)
